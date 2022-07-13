0cfb5abdae2636d7aa5d8c88ef268a81018c6afd
pedrokpdo <pedromotoehm@gmail.com>
2022-07-13 12:40:55 -0300

    update

--- jogo.js
+++ jogo.js
@@ -1,3 +1,5 @@
+let frames = 0
+
 const sprites = new Image();
 sprites.src = './sprites.png';
 
@@ -17,12 +19,12 @@ const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
 
 
-function criaPerson () {
+function criaPerson() {
   const person = {
     spriteX: 100,
     spriteY: 85,
-    largura: 97,
-    altura: 168,
+    largura: 131,
+    altura: 155,
     x: 10,
     y: 315,
     width: 80,
@@ -51,10 +53,26 @@ function criaPerson () {
         person.y = person.y + person.velocidade
       }
     },
+    movimentos: [
+      { spriteX: 1179, spriteY: 87, },
+      { spriteX: 900, spriteY: 87, },
+      { spriteX: 608, spriteY: 87, },
+      { spriteX: 341, spriteY: 87, },
+      { spriteX: 83, spriteY: 87, },
+    ],
+    frameAtual: 0,
+    atualizaOFrame() {
+      const baseDoIncremento = 1;
+      const incremento = baseDoIncremento + person.frameAtual;
+      const baseRepeticao = person.movimentos.length;
+      setTimeout(() => {person.frameAtual = incremento % baseRepeticao;}, 50)
+    },
     desenha() {
+      person.atualizaOFrame()
+      const { spriteX, spriteY } = person.movimentos[person.frameAtual]
       contexto.drawImage(
         sprites,
-        person.spriteX, person.spriteY,
+        spriteX, spriteY,
         person.largura, person.altura,
         person.x, person.y,
         person.width, person.height
@@ -64,6 +82,65 @@ function criaPerson () {
   return person
 }
 
+function criaChao() {
+  const chao = {
+    spriteX: 0,
+    spriteY: 0,
+    largura: 1600,
+    altura: 800,
+    x: 0,
+    y: 0,
+    atualiza() {
+      console.log('chao se moe')
+      const movimentoDoChao = 1;
+      const repeteEm = chao.largura / 2
+      const movimentacao = chao.x - movimentoDoChao
+
+      console.log(chao.x)
+      console.log(repeteEm)
+      chao.x = movimentacao % repeteEm
+    },
+    width: 2000,
+    height: canvas.height,
+    desenha() {
+      contexto.drawImage(
+        spriteChao,
+        chao.spriteX, chao.spriteY,
+        chao.largura, chao.altura,
+        chao.x, chao.y,
+        chao.width, chao.height
+      )
+    }
+  }
+  return chao
+}
+
+function criaBarril( ) {
+  const barril = {
+    spriteX: 0,
+    spriteY: 0,
+    largura: 360,
+    altura: 403,
+    x: 300,
+    y: 325,
+    width: 60,
+    height: 60,
+    velocidade: 0,
+    desenha() {
+      contexto.drawImage(
+        spriteBarril,
+        barril.spriteX, barril.spriteY,
+        barril.largura, barril.altura,
+        barril.x, barril.y,
+        barril.width, barril.height
+      )
+    },
+    atualiza() {
+      
+    }
+  }
+  return barril
+}
 
 const telaInicial = {
   spriteX: 0,
@@ -86,59 +163,13 @@ const telaInicial = {
 }
 const globais = {}
 
-const barril = {
-  spriteX: 0,
-  spriteY: 0,
-  largura: 360,
-  altura: 403,
-  x: 300,
-  y: 325,
-  width: 60,
-  height: 60,
-  velocidade: 0,
-  desenha() {
-    contexto.drawImage(
-      spriteBarril,
-      barril.spriteX, barril.spriteY,
-      barril.largura, barril.altura,
-      barril.x, barril.y,
-      barril.width, barril.height
-    )
-  },
-  atualiza() {
-    barril.velocidade = barril.velocidade + 1;
-    barril.x = barril.x - 6
-  }
-}
-
-const chao = {
-  spriteX: 0,
-  spriteY: 0,
-  largura: 1600,
-  altura: 800,
-  x: 0,
-  y: 0,
-  width: canvas.width,
-  height: canvas.height,
-  desenha() {
-    contexto.drawImage(
-      spriteChao,
-      chao.spriteX, chao.spriteY,
-      chao.largura, chao.altura,
-      chao.x, chao.y,
-      chao.width, chao.height
-    )
-  }
-}
-
-
 
 
 let telaAtiva = {}
 function mudaParaTela(novaTela) {
   telaAtiva = novaTela
 
-  if(telaAtiva.inicializa) {
+  if (telaAtiva.inicializa) {
     telaAtiva.inicializa()
   }
 }
@@ -160,24 +191,28 @@ const Telas = {
 Telas.JOGO = {
   inicializa() {
     globais.person = criaPerson()
+    globais.chao = criaChao()
+    globais.barril = criaBarril()
   },
   desenha() {
-    chao.desenha()
+    globais.chao.desenha()
     globais.person.desenha()
-    barril.desenha()
+    globais.barril.desenha()
   },
   click() {
     globais.person.pula()
   },
   atualiza() {
     globais.person.atualiza()
-    barril.atualiza()
+    globais.barril.atualiza()
+    globais.chao.atualiza()
   }
 }
 
 function loop() {
   telaAtiva.desenha()
   telaAtiva.atualiza()
+  frames = frames + 1
 
   requestAnimationFrame(loop)
 
